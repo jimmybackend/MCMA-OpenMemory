0a8a63f72645651f56748fc4788bd31faabb4059
Jimmy Backend <jimmybackend@gmail.com>
2026-09-01 14:11:38 -0600

    test: cover favicon and logout refresh

--- tests/integration/web-static-base-path.php
+++ tests/integration/web-static-base-path.php
@@ -12,6 +12,7 @@ $app=file_get_contents($root.'/app.js');
 $admin=file_get_contents($root.'/admin.html');
 
 check(is_string($index),'index.html readable');
+check(str_contains($index,'href="favicon.svg"'),'favicon is base-path relative');
 check(str_contains($index,'href="app.css"'),'index stylesheet is base-path relative');
 check(str_contains($index,'href="admin.html"'),'admin link is base-path relative');
 check(str_contains($index,'href="login"'),'login link is base-path relative');
@@ -19,7 +20,7 @@ check(str_contains($index,'src="app.js"'),'app script is base-path relative');
 
 check(is_string($app),'app.js readable');
 check(str_contains($app,"fetch('logout'"),'logout endpoint is base-path relative');
-check(str_contains($app,"location.href='./'"),'logout return is base-path relative');
+check(str_contains($app,"location.replace('./?signed_out=1')"),'logout refresh is base-path relative');
 
 check(is_string($admin),'admin.html readable');
 check(str_contains($admin,'href="app.css"'),'admin stylesheet is base-path relative');
